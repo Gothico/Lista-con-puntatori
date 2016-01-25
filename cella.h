fa6c7fb331010a85b495e2bfe651e59c92039ce0
Gothico <frusciante2000@hotmail.it>
2016-01-25 12:13:59 +0100

    Create cella.h

---
+++ cella.h
@@ -0,0 +1,30 @@
+/*
+ *Quest'opera è stata rilasciata con licenza Creative Commons Attribuzione - Non commerciale - 
+ *Condividi allo stesso modo 3.0 Italia. Per leggere una copia della licenza visita il 
+ *sito web http://creativecommons.org/licenses/by-nc-sa/3.0/it/  
+ *o spedisci una lettera a Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ *
+ * Classe cella: contiene l'astrazione relativa alla cella
+ *
+ * Autore: Riccardo Monterisi www.riccardomonterisi.tk
+ */
+
+#ifndef CELLA_H_
+#define CELLA_H_
+
+template <class tipoelem>
+class Cella{
+public:
+	tipoelem dato;
+	Cella<tipoelem> *prec;
+	Cella<tipoelem> *succ;
+
+};
+/*
+ * da cambiare ogni volta
+ */
+typedef Cella<float>* posizione;
+
+typedef Cella<float>* NODO;
+
+#endif /* CELLA_H_ */
